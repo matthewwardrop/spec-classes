fb412534c252bbbe48d5c88831d4c64073d83eba
Matthew Wardrop <mpwardrop@gmail.com>
2021-09-02 11:11:54 -0700

    fix for adding Attr

--- spec_classes/types/__init__.py
+++ spec_classes/types/__init__.py
@@ -1,3 +1,4 @@
+from .attr import Attr
 from .attr_proxy import AttrProxy
 from .keyed import KeyedList, KeyedSet
 from .missing import MISSING
@@ -5,6 +6,7 @@ from .spec_property import spec_property
 from .validated import ValidatedType, bounded, validated
 
 __all__ = (
+    "Attr",
     "AttrProxy",
     "KeyedList",
     "KeyedSet",
