4f8cfe1e38a8a86fa0eca1e3ede892b12aa8b714
removedporn <86824510+removedporn@users.noreply.github.com>
2022-07-25 13:42:55 +0800

    Delete spec_classes directory

--- spec_classes/methods/collections/__init__.py
+++
@@ -1,9 +0,0 @@
-from .mappings import MAPPING_METHODS
-from .sequences import SEQUENCE_METHODS
-from .sets import SET_METHODS
-
-__all__ = (
-    "MAPPING_METHODS",
-    "SEQUENCE_METHODS",
-    "SET_METHODS",
-)
